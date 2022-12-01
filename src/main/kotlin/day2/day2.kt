36b8c993b88639e603794e1b9ae96397d38f6007
KristofferFJ <kristofferfage@gmail.com>
2022-12-01 22:33:38 +0100

    prepare day2

---
+++ src/main/kotlin/day2/day2.kt
@@ -0,0 +1,20 @@
+package day2
+
+import utils.FileReader
+
+fun doStuff() {
+    val file = FileReader.readAsLines("day2")
+}
+
+fun partOne() {
+    println(doStuff())
+}
+
+fun partTwo() {
+    //todo
+}
+
+fun main() {
+    partOne()
+    partTwo()
+}
