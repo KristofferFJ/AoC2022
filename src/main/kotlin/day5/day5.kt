da98c8096649f1b9509082e7f9a53624a0f04e61
KristofferFJ <kristofferfage@gmail.com>
2022-12-04 11:19:42 +0100

    prepare day5

---
+++ src/main/kotlin/day5/day5.kt
@@ -0,0 +1,16 @@
+package day5
+
+import utils.FileReader
+
+val input = FileReader.readAsLines("day5")
+
+fun partOne() {
+}
+
+fun partTwo() {
+}
+
+fun main() {
+    println(partOne())
+    println(partTwo())
+}
