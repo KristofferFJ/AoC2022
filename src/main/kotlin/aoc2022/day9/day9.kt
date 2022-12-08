7eb1793eb97b141de2b3bb9d12321dd7712bf02c
kfj <kristofferfage@gmail.com>
2022-12-08 19:00:00 +0100

    prepare day9

---
+++ src/main/kotlin/aoc2022/day9/day9.kt
@@ -0,0 +1,16 @@
+package aoc2022.day9
+
+fun main() {
+    fun partOne() {
+        val hest = inputtest
+    }
+
+    fun partTwo() {
+    }
+
+    partOne()
+    partTwo()
+}
+
+const val inputtest = """"""
+const val input = """"""
