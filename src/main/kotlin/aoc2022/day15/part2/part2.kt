3e68e207e1b97a535b9583120d453e17997ea10f
kfj <kristofferfage@gmail.com>
2022-12-16 18:44:13 +0100

    day15 done

--- src/main/kotlin/aoc2022/day15/part2/part2.kt
+++ src/main/kotlin/aoc2022/day15/part2/part2.kt
@@ -2,24 +2,67 @@ package aoc2022.day15.part2
 
 import kotlin.math.absoluteValue
 
-const val maxSize = 400000
+const val maxSize = 4000000
+
 data class Point(val x: Int, val y: Int)
 data class SensorBeacon(val sensor: Point, val beacon: Point) {
-    fun getCoveredPointsOnLine(line: Int): Set<Int> {
-        val distance = (sensor.x - beacon.x).absoluteValue + (sensor.y - beacon.y).absoluteValue
-        val xMax = distance - (sensor.y - line).absoluteValue
-        if(xMax < 1) return emptySet()
-        return IntRange(maxOf(-xMax + sensor.x, 0), minOf(xMax + sensor.x, maxSize)).toSet()
+    private val viewingDistance = (sensor.x - beacon.x).absoluteValue + (sensor.y - beacon.y).absoluteValue
+
+    fun getCoveredPointsOnLine(line: Int): Interval? {
+        val viewingDistance = viewingDistance
+        val distanceToLine = (sensor.y - line).absoluteValue
+        val maxSpotsToCover = viewingDistance - (sensor.y - line).absoluteValue
+        if (maxSpotsToCover < 1) return null
+        val from = maxOf(sensor.x - (viewingDistance - distanceToLine), -1)
+        val to = minOf(sensor.x + (viewingDistance - distanceToLine), maxSize + 1)
+        return Interval(minOf(from, maxSize + 1), maxOf(to, -1))
     }
 }
 
-fun getAvailablePointsOnLine(sensorBeacons: List<SensorBeacon>, y: Int): Set<Int> {
-    val availableValues = IntRange(0, maxSize).toMutableSet()
-    sensorBeacons.forEach {
-        availableValues.removeAll(it.getCoveredPointsOnLine(y))
-        if(availableValues.isEmpty()) return emptySet()
+data class Interval(var start: Int, var end: Int) {
+    fun intersects(interval: Interval): Boolean {
+        return interval.start <= this.end + 1 && this.start <= interval.end + 1
     }
-    return availableValues
+}
+data class CoveredIntervals(val intervals: MutableList<Interval> = mutableListOf()) {
+    fun addIntervalAndCheckOverlap(interval: Interval) {
+        val intersectingInterval = this.intervals.find { it.intersects(interval) }
+        if(intersectingInterval != null) {
+            intersectingInterval.start = minOf(interval.start, intersectingInterval.start)
+            intersectingInterval.end = maxOf(interval.end, intersectingInterval.end)
+            spliceIntervals(intersectingInterval)
+            return
+        }
+        this.intervals.add(interval)
+    }
+
+    private fun spliceIntervals(newInterval: Interval) {
+        val intersectingInterval = this.intervals.find { it.intersects(newInterval) && it != newInterval }
+        if(intersectingInterval != null) {
+            intersectingInterval.start = minOf(newInterval.start, intersectingInterval.start)
+            intersectingInterval.end = maxOf(newInterval.end, intersectingInterval.end)
+            this.intervals.remove(newInterval)
+            return spliceIntervals(intersectingInterval)
+        }
+    }
+
+    fun isComplete(): Boolean {
+        return this.intervals.size == 1 && this.intervals[0].start == -1 && this.intervals[0].end == maxSize + 1
+    }
+}
+
+fun getAvailablePointsOnLine(sensorBeacons: List<SensorBeacon>, y: Int) {
+    val intervals = CoveredIntervals()
+    sensorBeacons.forEach {
+        val coveredPoints = it.getCoveredPointsOnLine(y) ?: return@forEach
+        intervals.addIntervalAndCheckOverlap(coveredPoints)
+        if(intervals.isComplete()) {
+            return
+        }
+    }
+    println(
+        (intervals.intervals[0].end + 1).toLong() * 4000000L + y
+    )
 }
 
 fun main() {
@@ -39,13 +82,7 @@ fun main() {
         )
     }
     IntRange(0, maxSize).forEach {
-        if(getAvailablePointsOnLine(sensorBeacons, it).isNotEmpty()){
-            println(it)
-            println(getAvailablePointsOnLine(sensorBeacons, it))
-            if(it % 10000 == 0) {
-                println(it)
-            }
-        }
+        getAvailablePointsOnLine(sensorBeacons, it)
     }
 }
 
